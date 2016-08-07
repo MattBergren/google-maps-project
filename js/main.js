474af2ac8e3abd6edf6ab86dce3d6e26f2957df8
Matthew Bergren <matt@Hal.local>
2016-08-07 09:24:56 -0500

    early stages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
-$(document).ready(function() {
-	console.log('hello-console');
+var map;
+function initMap() {
+	
+	// Constructor creates a new map - only center and zoom are required.
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {
+			lat: 40.7413549,
+			lng: -73.9980244
+		},
+		zoom: 13
+	});
 
-});
+	var tribeca = {
+		lat: 40.719526,
+		lng: -74.0089934
+	}
+
+	var marker = new google.maps.Marker({
+		position: tribeca,
+		map: map,
+		title: 'First Marker'
+	});
+
+	var infowindow = new google.maps.InfoWindow({
+		content: 'Do you ever feel like an InfoWindow'
+	});
+
+	// Add event listener to open an infowindow
+
+	marker.addListener('click', function() {
+		infowindow.open(map, marker);
+	});
+
+
+}
